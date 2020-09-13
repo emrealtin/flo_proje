f3a8369c40b2eeb54117f357d44159b258f24404
Emre Altın <iletisim@emrealtin.com.tr>
2020-09-13 20:05:54 +0300

    create models

---
+++ app/StockInfo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StockInfo extends Model
+{
+    protected $table = 'stock_info';
+}
---
+++ app/WareHouseProducts.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WareHouseProducts extends Model
+{
+    protected $table = 'warehouse_products';
+}
