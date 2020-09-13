0089d03afe6de7be1e99659174673075eb1edd8c
Emre Altın <iletisim@emrealtin.com.tr>
2020-09-13 21:03:31 +0300

    create stock_info table faker seeder

---
+++ database/seeds/StockInfoSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Faker\Factory as Faker;
+use App\StockInfo;
+use Illuminate\Database\Seeder;
+
+class StockInfoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach (range(1,10) as $index) {
+            StockInfo::insert([
+                'barcode' => $faker->ean13,
+                'warehouse_id' => $faker->numberBetween(1,10),
+                'quantity' => $faker->randomDigit,
+                'process_type' => $faker->numberBetween(1,2),
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null)
+            ]);
+        }
+    }
+}
