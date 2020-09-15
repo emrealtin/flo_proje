7dad2d8c5425c4860abd20d51d1cbe74dc6ac722
Emre Altın <iletisim@emrealtin.com.tr>
2020-09-15 22:58:58 +0300

    order table add column order_id

---
+++ database/migrations/2020_09_15_193333_add_order_id_column_order_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOrderIdColumnOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->integer('order_id')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
